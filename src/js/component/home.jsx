e7600b35778ee277ad7252c38cf8fe7ade7038a6
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-05-14 03:36:52 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState([
+		
+	]);
+
+	const [inputValue, setInputValue] = useState ("");
+	
+	const controlarInput = (e) => {
+		setInputValue(e.target.value) 
+	}
+	const controlarEnter = (e) => {
+		if (e.key === 'Enter') {
+			setTareas(tareas.concat(inputValue));
+			setInputValue ("");
+		}
+	}
+	
+	const deleteItems = (e) => {
+		setTareas((prevState) =>
+		  prevState.filter((tarea, index) => index !== e)
+		);
+	  };
+	
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="text-center txt-light my-4">
+				<h1><strong>Todo LIST</strong></h1>
+				<ul>
+					<li><input className= "form-control m-auto" type="text" onChange={controlarInput} onKeyDown={controlarEnter} value={inputValue} placeholder="Ojalá funcione" ></input> </li>
+					{tareas.map((t, index) => <li key={index}>{t} <button onClick={() => deleteItems (index)}><i className="fa-solid fa-trash"></i></button></li>)}
+			
+				</ul>	
+			</div>
+
+				<div className="text-center text-bold my-4 total"> {tareas.length} tasks </ div>
+
 		</div>
 	);
 };
